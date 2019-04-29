56e12d72f24ecba4976ff91e2cade20231b2cb0e
unknown <512286986@qq.com>
2019-04-29 17:39:16 +0800

    增加垃圾回收器总结

---
+++ jvm/src/J02stringtest/StringTest.java
@@ -0,0 +1,11 @@
+package J02stringtest;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 400;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
