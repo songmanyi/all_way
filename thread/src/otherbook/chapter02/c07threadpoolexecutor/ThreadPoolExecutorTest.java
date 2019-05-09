5e8e91f64ddc9f62b4aece4560fb31edcce52100
unknown <512286986@qq.com>
2019-05-09 14:24:51 +0800

    增加多线程相关的学习总结

---
+++ thread/src/otherbook/chapter02/c07threadpoolexecutor/ThreadPoolExecutorTest.java
@@ -0,0 +1,23 @@
+package otherbook.chapter02.c07threadpoolexecutor;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolExecutorTest {
+
+    public static void main(String[] args) {
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5));
+
+        for(int i=0;i<15;i++){
+            MyThread myTask = new MyThread(i);
+            executor.execute(myTask);
+            System.out.println("线程池中线程数目："+executor.getPoolSize()+"，队列中等待执行的任务数目："+
+                    executor.getQueue().size()+"，已执行玩别的任务数目："+executor.getCompletedTaskCount());
+        }
+        executor.shutdown();
+
+    }
+
+}
